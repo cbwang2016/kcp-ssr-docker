fa9e4e6f76c7049642b21f0404909fd5a61844f7
cbwang2016 <cbwang2016@users.noreply.github.com>
2016-10-16 15:10:12 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 echo "{"\"server\":\"$server\"","\"server_ipv6\":\"$server_ipv6\"","\"server_port\":\"$server_port\"","\"password\":\"$password\"","\"timeout\":120","\"udp_timeout\":60","\"method\":\"$method\"","\"protocol\":\"$protocol\"","\"obfs\":\"$obfs\"","\"dns_ipv6\":false","\"connect_verbose_info\":0","\"redirect\":\"\"","\"fast_open\":true"}" > /config.json
 /usr/bin/supervisord
